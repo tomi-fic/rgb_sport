8206d2eae353182b11abd81694d8cdd205aed419
tomi-fic <59510664+tomi-fic@users.noreply.github.com>
2020-01-25 11:21:52 +0100

    Adding service workflow
    - canvas line js
    - hover, active bootstrap effects

--- js/script.js
+++ js/script.js
@@ -15,3 +15,56 @@ ELEMENTS.forEach((element, index) => {
 		ELEMENTS_SPAN[index].style.top = e.pageY - element.offsetTop + "px";
 	});
 });
+
+var c = document.getElementById("lineDraw");
+window.addEventListener("resize", drawFunction);
+drawFunction();
+
+function drawFunction() {
+	var screenWidth = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+	if (screenWidth < 950) {c.width  = 300;c.height = 670;}
+					  else {c.width  = 790;c.height = 450;}
+	var ctx = c.getContext("2d");
+	ctx.clearRect(0, 0, c.width, c.height);
+	ctx.beginPath();
+	ctx.setLineDash([10, 15]);
+	ctx.lineWidth = 5;
+	ctx.lineCap = 'round';
+	ctx.strokeStyle = "purple"; // OBJ - PICK
+	if (screenWidth < 950) {
+		ctx.moveTo(80, 10);
+		ctx.bezierCurveTo(-20,100,0,300,270,400);
+	} else {
+		ctx.moveTo(70, 150);
+		ctx.bezierCurveTo(280,200,200,500,450,290);
+	}
+	ctx.stroke();
+
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+	ctx.setLineDash([10, 15]);
+	ctx.lineWidth = 5;
+	ctx.strokeStyle = "purple"; // DEL - PICK
+	if (screenWidth < 950) {
+		ctx.moveTo(250, 170);
+		ctx.bezierCurveTo(100,250,350,300,250,370);
+	} else {
+		ctx.moveTo(370, 50);
+		ctx.bezierCurveTo(650,200,100,150,450,320);
+	}
+	ctx.stroke();
+
+	var ctx = c.getContext("2d");
+	ctx.beginPath();
+	ctx.setLineDash([10, 15]);
+	ctx.lineWidth = 5;
+	ctx.strokeStyle = "purple"; // SERV - PICK
+	if (screenWidth < 950) {
+		ctx.moveTo(250, 380);
+		ctx.bezierCurveTo(50,400,100,480,120,570);
+	} else {
+		ctx.moveTo(720, 120); // SERV - PICK
+		ctx.bezierCurveTo(520,400,500,270,450,350);
+	}
+	ctx.stroke();
+}
