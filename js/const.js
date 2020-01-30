80f6464f86cf767e855c01dcfe9fd5365e6be2b9
tomi-fic <59510664+tomi-fic@users.noreply.github.com>
2020-01-30 19:16:45 +0100

    Adding pricelist with react

---
+++ js/const.js
@@ -0,0 +1,164 @@
+var const_dat =
+[
+  {
+    id: 1,
+    name: "Centrovanie kolesa",
+    cena: "5,50",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 2,
+    name: "Vypletenie kolesa s centrovaním",
+    cena: "13,50",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 3,
+    name: "Výmena duše",
+    cena: "3,90",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 4,
+    name: "Zalepenie defektu",
+    cena: "3,00",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 5,
+    name: "Premazanie náboja, stredu, hlavového obloženia",
+    cena: "7,90",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 6,
+    name: "Nastavenie prehadzovačov",
+    cena: "5,00",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 7,
+    name: "Výmena lanka a nastavenie prehadzovačov",
+    cena: "6,90",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 8,
+    name: "Nastavenie brzdy",
+    cena: "3,00",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 9,
+    name: "Výmena lanka a nastavenie brzdy",
+    cena: "5,90",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 10,
+    name: "Odvzdušnenie hydraulickej brzdy",
+    cena: "10,50",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 11,
+    name: "Výmena brzdových platničiek",
+    cena: "4,90",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 12,
+    name: "Výmena reťaze",
+    cena: "3,50",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 13,
+    name: "Premazanie odpruženej vidlice",
+    cena: "14,90",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 14,
+    name: "Montáž omotávky na riadidlá",
+    cena: "6,50",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 15,
+    name: "Montáž a nastavenie tachometra",
+    cena: "3,90",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 16,
+    name: "Montáž nosiča",
+    cena: "4,50",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 17,
+    name: "Opieskovanie a lakovanie rámu",
+    cena: "59,50",
+    desc: "",
+    group: "SM"
+  },
+  {
+    id: 18,
+    name: "VYSKLADANIE BICYKLA",
+    cena: "50,00",
+    desc: "vyskladanie bicykla z dodaných súčiastok, kompletné nastavenie",
+    group: "BG"
+  },
+  {
+    id: 19,
+    name: "PREDPREDAJNÝ SERVIS BICYKLA",
+    cena: "25,00",
+    desc: "skompletizovanie, poskladanie a príprava bicykla na bežnú prevádzku, kompletné nastavenie",
+    group: "BG"
+  },
+  {
+    id: 20,
+    name: "KOMPLETNÁ REPASÁŽ",
+    cena: "35,00",
+    desc: "premazanie všetkých ložísk (okrem pedálov), premazanie laniek bŕzd a radenia, nastavenie bŕzd a prehadzovačov, celková kontrola bicykla, centrovanie kolies",
+    group: "BG"
+  },
+  {
+    id: 21,
+    name: "PREHLIADKA A NASTAVENIE BICYKLA",
+    cena: "25,00",
+    desc: "kontrola a dotiahnutie všetkých skrutiek a spojov bicykla, montáž/demontáž kolies, nastavenie vôle nábojov kolies, stredového a hlavového zloženia, kompletné nastavenie radenia a bŕzd, mazanie reťaze, prehadzovača, prešmykovača, kontrola/nastavenie pätky prehadzovača - diagnostika prípadných ďalších závad",
+    group: "BG"
+  },
+  {
+    id: 22,
+    name: "PREHLIADKA JEDNORÝCHLOSTNÉHO BICYKLA",
+    cena: "18,00",
+    desc: "kontrola a dotiahnutie všetkých skrutiek a spojov bicykla, nastavenie vôle nábojov kolies, stredového a hlavového zloženia, kompletné nastavenie radenia a bŕzd, mazanie reťaze",
+    group: "BG"
+  },
+  {
+    id: 23,
+    name: "ČISTENIE BICYKLA",
+    cena: "15,00",
+    desc: "Ručné povrchové čistenie bicykla (Je podmienené následným kompletným servisom)",
+    group: "BG"
+  }
+]
